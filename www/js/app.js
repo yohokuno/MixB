d205f7f4d8aacdb3ba15b19d7be80c6ad51bdfd2
Yoh Okuno <yoh@swiftkey.com>
2015-06-16 12:27:03 +0100

    refactoring

--- www/js/app.js
+++ www/js/app.js
@@ -12,61 +12,64 @@ app.run(function($ionicPlatform) {
 });
 
 app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDelegate, $ionicLoading, $rootScope) {
+  // wrap MixB's arbitrary URL rule
   function getUrl(country, category, action, id) {
-    var suffix = category + "/";
+    var suffix = category + '/';
     if (action) {
-      suffix += category + "_" + action;
+      suffix += category + '_' + action;
       if (!id) {
-        suffix += "_f.php";
+        suffix += '_f.php';
       } else {
-        suffix += "_fs.php?id=" + id;
+        suffix += '_fs.php?id=' + id;
       }
     }
-    if (country == "uk") {
+    if (country == 'uk') {
       switch (category) {
-        case "ser": case "cir": case "inf":
-          return "http://www.mixb.net/" + suffix;
+        case 'ser': case 'cir': case 'inf':
+          return 'http://www.mixb.net/' + suffix;
         default:
-          return "http://www.mixb.jp/uk/" + suffix;
+          return 'http://www.mixb.jp/uk/' + suffix;
       }
     }
-    return "http://" + country + ".mixb.net/" + suffix;
+    return 'http://' + country + '.mixb.net/' + suffix;
   }
 
+  // Main model data for countries
   $scope.countries = [
-    {name: "イギリス", id: "uk"},
-    {name: "フランス", id: "fra"},
-    {name: "ドイツ", id: "ger"},
-    {name: "イタリア", id: "ita"},
-    {name: "アイルランド", id: "irl"},
-    {name: "ニューヨーク", id: "nyc"},
-    {name: "ロサンゼルス", id: "los"},
-    {name: "サンフランシスコ", id: "sfc"},
-    {name: "カナダ・バンクーバー", id: "van"},
-    {name: "オーストラリア・シドニー", id: "syd"},
-    {name: "ニュージーランド", id: "nz"},
-    {name: "シンガポール", id: "sin"},
-    {name: "上海", id: "sha"},
-    {name: "香港", id: "hkg"},
+    {name: 'イギリス', id: 'uk'},
+    {name: 'フランス', id: 'fra'},
+    {name: 'ドイツ', id: 'ger'},
+    {name: 'イタリア', id: 'ita'},
+    {name: 'アイルランド', id: 'irl'},
+    {name: 'ニューヨーク', id: 'nyc'},
+    {name: 'ロサンゼルス', id: 'los'},
+    {name: 'サンフランシスコ', id: 'sfc'},
+    {name: 'カナダ・バンクーバー', id: 'van'},
+    {name: 'オーストラリア・シドニー', id: 'syd'},
+    {name: 'ニュージーランド', id: 'nz'},
+    {name: 'シンガポール', id: 'sin'},
+    {name: '上海', id: 'sha'},
+    {name: '香港', id: 'hkg'},
   ];
 
+  // Add categories to all countries
   $.each($scope.countries, function(i, country) {
     country.categories = [
-        {name: "住まい", id: "acm", items: []},
-        {name: "求人", id: "job", items: []},
-        {name: "売ります", id: "sal", items: []},
-        {name: "買います", id: "buy", items: []},
-        {name: "レッスン", id: "les", items: []},
-        {name: "サービス", id: "ser", items: []},
-        {name: "サークル ", id: "cir", items: []},
-        {name: "お知らせ", id: "inf", items: []},
+        {name: '住まい', id: 'acm', items: [], query: ''},
+        {name: '求人', id: 'job', items: [], query: ''},
+        {name: '売ります', id: 'sal', items: [], query: ''},
+        {name: '買います', id: 'buy', items: [], query: ''},
+        {name: 'レッスン', id: 'les', items: [], query: ''},
+        {name: 'サービス', id: 'ser', items: [], query: ''},
+        {name: 'サークル ', id: 'cir', items: [], query: ''},
+        {name: 'お知らせ', id: 'inf', items: [], query: ''},
     ];
   });
 
   $scope.activeCountry = 0;     // select UK by default
   $scope.activeCategory = 0;    // select acm by default
-  $scope.search = {query: ""};
 
+  // helper function to show error message
   function handleError(url) {
     $ionicLoading.show({
       template: '読み込めませんでした：' + url,
@@ -75,12 +78,12 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
     });
   }
 
-  $scope.updateItems = function(index) {
-    $scope.activeCategory = index;
+  // update items in active category of active country
+  $scope.updateItems = function() {
     var country = $scope.countries[$scope.activeCountry];
-    var category = country.categories[index];
+    var category = country.categories[$scope.activeCategory];
     var dirname = getUrl(country.id, category.id);
-    var url = getUrl(country.id, category.id, "list");
+    var url = getUrl(country.id, category.id, 'list');
 
     $http.get(url).success(function(data) {
       var contents = $(data).find('table > tbody > tr > td > table > tbody > tr > td > table');
@@ -97,36 +100,40 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
   };
 
   // TODO: merge with updateItems()
-  $scope.searchItem = function(query) {
+  $scope.searchItem = function() {
     var country = $scope.countries[$scope.activeCountry];
     var category = country.categories[$scope.activeCategory];
     var dirname = getUrl(country.id, category.id);
-    var url = getUrl(country.id, category.id, "search");
+    var url = getUrl(country.id, category.id, 'search');
 
     $ionicLoading.show({template: '<ion-spinner></ion-spinner>', noBackdrop: true})
-    $scope.search.query = "";
 
     $http({
           method: 'POST',
           url: url,
-          data: $.param({'sc_word':query}),
+          data: $.param({'sc_word':category.query}),
           headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     }).success(function(data) {
       var contents = $(data).find('table > tbody > tr > td > table > tbody > tr > td > table');
       console.log(contents.html());
       var rows = contents.find('tbody > tr').filter(function(i,e) {return $(e).find('td > a').length == 1});
+
       category.items = rows.map(function(i,e) {
         return {
           title: $(e).find('td > a').text(),
           url: dirname + $(e).find('td > a').attr('href'),
         };
       }).get();
+
       $ionicLoading.hide();
       $rootScope.$broadcast('scroll.refreshComplete');
+
     }).error(function() {handleError(url);});
+
+    category.query = '';
   };
 
-  // modal view for item detail
+  // Add modal view for item detail
   $ionicModal.fromTemplateUrl('item-detail.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -134,6 +141,7 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
     $scope.modal = modal
   });
 
+  // Open item detail modal view
   $scope.openItemDetail = function(url) {
     var country = $scope.countries[$scope.activeCountry];
     var category = country.categories[$scope.activeCategory];
@@ -145,8 +153,8 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
       var contents = $(data).find('table > tbody > tr > td > table > tbody > tr > td > table');
       // replace image path to absolute url
       contents.find('img').each(function() {
-        var src = $(this).attr("src");
-        $(this).attr("src", dirname + src);
+        var src = $(this).attr('src');
+        $(this).attr('src', dirname + src);
       });
       $scope.title = contents.find('tr:eq(0)').text();
       $scope.metadata = contents.find('tr:eq(1)').html();
@@ -159,19 +167,21 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
     }).error(function() {handleError(url);});
   };
 
+  // Pressed back button on item detail view
   $scope.closeItemDetail = function() {
     $scope.modal.hide();
   };
 
-  // functions for multiple countries
+  // Selected country in side menu
   $scope.selectCountry = function(country, index) {
     $scope.activeCountry = index;
     $ionicSideMenuDelegate.toggleLeft(false);
     $scope.activeCategory = 0;
-    $scope.updateItems($scope.activeCategory);
-    $ionicLoading.show({template: '<ion-spinner></ion-spinner>', noBackdrop: true})
+    $scope.showLoading();
+    $scope.updateItems();
   };
 
+  // Show/hide side menu
   $scope.toggleCountries = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
@@ -185,13 +195,15 @@ app.controller('MainCtrl', function($scope, $http, $ionicModal, $ionicSideMenuDe
     }
   };
 
+  // Slide changed by swiping slide or tapping tab
   $scope.onSlideChanged = function(index) {
-    $scope.updateItems(index);
+    $scope.activeCategory = index;
     $scope.showLoading();
+    $scope.updateItems();
   };
 
   // Initialize
   $scope.showLoading();
-  $scope.updateItems($scope.activeCategory);
+  $scope.updateItems();
 });
 
